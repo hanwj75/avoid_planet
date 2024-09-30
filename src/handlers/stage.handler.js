ab36d4a6410cd48f43587f50397fb56e1abee779
hanwj75 <wjws9608@gmail.com>
2024-09-30 17:28:54 +0900

    [Update] stage.handler
    유저의 현재 스테이지의 정보를 알려줌

--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -1,4 +1,16 @@
+import { getStage } from "../models/stage.model.js";
+
 //다음 스테이지
 export const moveStageHandler = (userId, payload) => {
+  //유저는 스테이지를 하나씩 올라갈수있음 1->2  2->3
+  //유저는 일정 조건을 달성하면 다음 스테이지로 넘어감
+
+  //현재스테이지,다음스테이지 보내주기
+  //현재 스테이지 정보
+  let currentStages = getStage(userId);
+  if (!currentStages.length) {
+    return { status: "fail", message: "스테이지가 없습니다." };
+  }
+
   return { status: "success" };
 };
