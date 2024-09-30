e4501f6748bdb462bc63b6ea9ae8e7d7118f21df
hanwj75 <wjws9608@gmail.com>
2024-09-30 22:16:04 +0900

    [Update] game
    새 게임 시작시 이전데이터 삭제로직 추가

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,10 +1,13 @@
 import { getGameAssets } from "../init/assets.js";
-import { getStage, setStage } from "../models/stage.model.js";
+import { clearStage, getStage, setStage } from "../models/stage.model.js";
 
 export const gameStart = (uuid, payload) => {
   //접속하자마자 시작함
   //접속하자마자 스테이지의 정보를 넣어줘야함
   const { stages } = getGameAssets();
+
+  //새 게임 시작시 이전 데이터 삭제
+  clearStage(uuid);
   //stages 배열의 0번째 === 첫 스테이지
   setStage(uuid, stages.data[0].id, payload.timeStamp);
   console.log("스테이지:", getStage(uuid));
