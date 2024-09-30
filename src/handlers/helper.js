cec80c9cf18a2b40fcb2cecb0cc9442349bc5d6a
hanwj75 <wjws9608@gmail.com>
2024-09-30 13:13:02 +0900

    [Add] helper
    connection과 disconnect 핸들 추가,
    Event핸들러 호출시 버전 검증

---
+++ src/handlers/helper.js
@@ -0,0 +1,31 @@
+import { CLIENT_VERSION } from "../constants.js";
+import { getGameAssets } from "../init/assets.js";
+import { getStage, setStage } from "../models/stage.model.js";
+import { getUser, removeUser } from "../models/user.model.js";
+
+export const handleDisconnect = (socket, uuid) => {
+  removeUser(socket.id);
+  console.log(`유저 접속 종료:${socket.id}`);
+  console.log(`현재 접속중인 유저:`, getUser());
+};
+
+export const handlerConnection = (socket, uuid) => {
+  console.log(`새로운 유저 접속:${uuid} 의 socket ID :${socket.id}`);
+  console.log(`현재 접송중인 유저:`, getUser());
+
+  //접속하자마자 시작함
+  //접속하자마자 스테이지의 정보를 넣어줘야함
+  const { stages } = getGameAssets();
+  //stages 배열의 0번째 === 첫 스테이지
+  setStage(uuid, stages.data[0].id);
+  console.log("스테이지:", getStage(uuid));
+
+  socket.emit("connection", { uuid }); //connection발사
+};
+
+export const handlerEvent = (io, socket, data) => {
+  if (!CLIENT_VERSION.includes(data.clientVersion)) {
+    socket.emit("response", { status: "fail", message: "클라이언트 버전이 잘못됨" });
+    return;
+  }
+};
