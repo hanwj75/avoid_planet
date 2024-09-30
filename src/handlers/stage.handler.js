48a8c587ac00c8c2acc040680360fad0cfab01f6
hanwj75 <wjws9608@gmail.com>
2024-09-30 18:10:39 +0900

    [Update] stage.handler
    점수 검증 로직 추가 / currentStageId => currenStage 로 수정

--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -15,10 +15,19 @@ export const moveStageHandler = (userId, payload) => {
 
   //오름차순 -> 가장큰 스테이지 ID 확인 <-유저의 현재 스테이지
   currentStages.sort((a, b) => a.id - b.id);
-  const currentStageId = currentStages[currentStages.length - 1].id; //가장 마지막 스테이지 id
+  const currentStage = currentStages[currentStages.length - 1].id; //가장 마지막 스테이지 id
   //클라이언트 vs 서버 비교
-  if (currentStageId !== payload.currentStages) {
+  if (currentStage.id !== payload.currentStages) {
     return { status: "fail", message: "현재 스테이지 아님" };
+  }
+
+  //점수 검증 로직
+  const serverTime = Date.now(); // 현재 타임스탬프
+  const elapsedTime = (serverTime - currentStage.timeStamp) / 1000;
+  //1스테이지에서 2스테이지로 넘어가는 가정
+  //다음단계 넘어가는부분 과제
+  if (elapsedTime < 100 || elapsedTime > 105) {
+    return { status: "fail", message: "시간 초과" };
   }
 
   //target 스테이지 검증 -게임에셋에 존재하는가?
@@ -27,6 +36,6 @@ export const moveStageHandler = (userId, payload) => {
     return { status: "fail", message: "다음 스테이지가 없음" };
   }
 
-  setStage(userId, payload.targetStage);
+  setStage(userId, payload.targetStage, serverTime);
   return { status: "success" };
 };
