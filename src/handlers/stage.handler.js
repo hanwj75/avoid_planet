476177d8f7211e43293bc2d0ae468cb77221da7d
hanwj75 <wjws9608@gmail.com>
2024-09-30 17:38:43 +0900

    [Update] stage.handler
    다음 유저의 현재스테이지확인
    다음 스테이지가 게임에셋에 존재하는지 검증

--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -1,4 +1,5 @@
-import { getStage } from "../models/stage.model.js";
+import { getGameAssets } from "../init/assets.js";
+import { getStage, setStage } from "../models/stage.model.js";
 
 //다음 스테이지
 export const moveStageHandler = (userId, payload) => {
@@ -12,5 +13,20 @@ export const moveStageHandler = (userId, payload) => {
     return { status: "fail", message: "스테이지가 없습니다." };
   }
 
+  //오름차순 -> 가장큰 스테이지 ID 확인 <-유저의 현재 스테이지
+  currentStages.sort((a, b) => a.id - b.id);
+  const currentStageId = currentStages[currentStages.length - 1].id; //가장 마지막 스테이지 id
+  //클라이언트 vs 서버 비교
+  if (currentStageId !== payload.currentStages) {
+    return { status: "fail", message: "현재 스테이지 아님" };
+  }
+
+  //target 스테이지 검증 -게임에셋에 존재하는가?
+  const { stages } = getGameAssets();
+  if (!stages.data.some((stage) => stage.id === payload.targetStage)) {
+    return { status: "fail", message: "다음 스테이지가 없음" };
+  }
+
+  setStage(userId, payload.targetStage);
   return { status: "success" };
 };
